5baf5dc51c54c87902b3e195d469c93db1be92f8
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-02-08 00:55:31 +0400

    feat: sidebar wip

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Dashboard } from './components/dashboard/dashboard';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Sidebar,
+    Dashboard,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('smart-home');
