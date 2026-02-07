fe45e3cdc5669802428c03337d2e4649edf79ab1
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-02-08 01:39:21 +0400

    feat: sidebar layout

--- src/app/components/sidebar/sidebar-menu/sidebar-menu.ts
+++ src/app/components/sidebar/sidebar-menu/sidebar-menu.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-sidebar-menu',
-  imports: [],
+  standalone: true,
+  imports: [MatListModule, MatIconModule],
   templateUrl: './sidebar-menu.html',
   styleUrl: './sidebar-menu.scss',
 })
-export class SidebarMenu {
-
-}
+export class SidebarMenu {}
