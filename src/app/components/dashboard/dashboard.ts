6d929c02d2741c3596f4f80cfaf333adb6ca3553
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-02-10 01:41:38 +0400

    feat: content for two tabs

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { CardList } from '../card-list/card-list';
+import { TabId } from '../../../models/types';
+import { DASHBOARD_MOCK } from '../../../mock-data/dashboard.mock';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,4 +10,18 @@ import { CardList } from '../card-list/card-list';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard {
+  activeTabId = signal<TabId>('overview');
+
+  cards = computed(() => {
+    const tabId = this.activeTabId();
+    const tab = DASHBOARD_MOCK.tabs.find((t) => t.id === tabId);
+    return tab?.cards ?? [];
+  });
+
+  onTabChange(index: number) {
+    const tabId: TabId = index === 0 ? 'overview' : 'lights';
+
+    this.activeTabId.set(tabId);
+  }
+}
