48dc0baf4ac9ab28a74d7584e2c89dc66488987a
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-02-11 17:50:10 +0400

    feat: highlight active card directive

--- src/app/components/card-list/card/card.ts
+++ src/app/components/card-list/card/card.ts
@@ -4,16 +4,17 @@ import { CardModel } from '../../../../models/types';
 import { Sensor } from './sensor/sensor';
 import { Device } from './device/device';
 import { NgClass } from '@angular/common';
+import { ActiveCardDirective } from '../../../directives/active.directive';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule, NgClass, Sensor, Device],
+  imports: [MatCardModule, NgClass, Sensor, Device, ActiveCardDirective],
   templateUrl: './card.html',
   styleUrl: './card.scss',
 })
 export class Card {
   @Input({ required: true }) card!: CardModel;
-
   @Input() layoutClass = '';
+  @Input() isActive = false;
 }
