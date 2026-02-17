96567707255342862af944fe438e8cc9e249d158
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-02-17 16:52:32 +0400

    feat: add navigation to login

--- src/app/interceptors/unauth-handle-interceptor.ts
+++ src/app/interceptors/unauth-handle-interceptor.ts
@@ -3,16 +3,19 @@ import { TokenStorageService } from '../services/token-storage/token-storage';
 import { inject } from '@angular/core';
 import { AuthStateService } from '../services/auth-service/auth-state-service';
 import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const unauthHandleInterceptor: HttpInterceptorFn = (req, next) => {
   const tokenService = inject(TokenStorageService);
   const authService = inject(AuthStateService);
+  const router = inject(Router);
 
   return next(req).pipe(
     catchError((error) => {
       if (error.status === 401) {
         tokenService.clearToken();
         authService.setAuthenticated(false);
+        router.navigate(['/login']);
       }
       return throwError(() => error);
     }),
