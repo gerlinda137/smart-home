ef83a0ad8fc58650f7d3cc045d2994b9c1a5c5b4
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-02-17 22:27:06 +0400

    feat: auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './components/pages/login-page/login-page';
 import { NotFoundPage } from './components/pages/not-found-page/not-found-page';
 import { DashboardPage } from './components/pages/dashboard-page/dashboard-page';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   {
@@ -10,4 +11,5 @@ export const routes: Routes = [
   },
   { path: 'login', component: LoginPage },
   { path: '**', component: NotFoundPage },
+  { path: 'dashboard/:dashboardId/:tabId', component: DashboardPage, canActivate: [authGuard] },
 ];
