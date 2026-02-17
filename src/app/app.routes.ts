0f0b6577bd1530d72d5a3731782bdac935f9f87b
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-02-17 17:49:58 +0400

    feat: basic routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './components/pages/login-page/login-page';
+import { NotFoundPage } from './components/pages/not-found-page/not-found-page';
+import { DashboardPage } from './components/pages/dashboard-page/dashboard-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardPage,
+  },
+  { path: 'login', component: LoginPage },
+  { path: '**', component: NotFoundPage },
+];
