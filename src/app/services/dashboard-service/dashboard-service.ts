e74bc314a40a98bf744ef04c3f202ff915d461ee
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-02-18 02:19:47 +0400

    feat: dashboard api service

---
+++ src/app/services/dashboard-service/dashboard-service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DashboardData } from '../../../models/types';
+
+export interface Dashboard {
+  id: string;
+  title: string;
+  icon: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DashboardService {
+  private http = inject(HttpClient);
+
+  getDashboards(): Observable<Dashboard[]> {
+    return this.http.get<Dashboard[]>('/dashboards');
+  }
+
+  getDashboardById(id: string): Observable<DashboardData> {
+    return this.http.get<DashboardData>(`/dashboards/${id}`);
+  }
+}
