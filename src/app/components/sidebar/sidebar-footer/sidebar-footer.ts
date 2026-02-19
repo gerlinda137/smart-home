e8c776b7915b53829b67002abd7287e71b5cda87
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-02-19 19:58:22 +0400

    feat: sidebar logout

--- src/app/components/sidebar/sidebar-footer/sidebar-footer.ts
+++ src/app/components/sidebar/sidebar-footer/sidebar-footer.ts
@@ -1,14 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { AuthStateService } from '../../../services/auth-service/auth-state-service';
 import { AsyncPipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { TokenStorageService } from '../../../services/token-storage/token-storage';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar-footer',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, MatButtonModule, MatIconModule, MatMenuModule],
   templateUrl: './sidebar-footer.html',
   styleUrl: './sidebar-footer.scss',
 })
 export class SidebarFooter {
   userProfile$ = inject(AuthStateService).userProfile$;
+  authService = inject(AuthStateService);
+  tokenStorage = inject(TokenStorageService);
+  router = inject(Router);
+
+  logout() {
+    this.authService.setAuthenticated(false);
+    this.tokenStorage.clearToken();
+    this.router.navigate(['/login']);
+  }
 }
