c1be520bcdef70fc975b7b940db273f80f2a6755
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-02-20 18:44:51 +0400

    fix: fix dashboard tabs

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { CardList } from '../card-list/card-list';
 import { DashboardData, Tab } from '../../../models/types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatTabsModule, CardList],
+  imports: [CommonModule, MatTabsModule, CardList],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
@@ -15,9 +16,13 @@ export class Dashboard {
 
   @Output() tabSelected = new EventEmitter<string>();
 
-  getCurrentTabIndex(): number {
+  get selectedIndex(): number {
     return this.dashboard.tabs.findIndex((t) => t.id === this.currentTab.id);
   }
+
+  // getCurrentTabIndex(): number {
+  //   return this.dashboard.tabs.findIndex((t) => t.id === this.currentTab.id);
+  // }
 
   onTabChange(index: number): void {
     this.tabSelected.emit(this.dashboard.tabs[index].id);
