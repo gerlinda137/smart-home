b958cef04ae03b26cdc8429db67193cb3b5ef62e
Angelina Baranova <46976877+gerlinda137@users.noreply.github.com>
2026-03-03 03:52:12 +0400

    feat: implement add tab functionality in dashboard with input handling

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,30 +1,36 @@
-import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, Signal, signal } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { CardList } from '../card-list/card-list';
 import { DashboardData, Tab } from '../../../models/types';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, MatTabsModule, CardList],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    CardList,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard implements OnChanges {
   @Input() dashboard!: DashboardData;
   @Input() currentTab?: Tab;
+  @Input() isEditMode$!: Signal<boolean>;
 
   @Output() tabSelected = new EventEmitter<string>();
+  isAddingTab = signal(false);
 
   selectedIndex = 0;
-
-  // get selectedIndex(): number {
-  //   return this.dashboard.tabs.findIndex((t) => t.id === this.currentTab.id);
-  // }
-
-  // getCurrentTabIndex(): number {
-  //   return this.dashboard.tabs.findIndex((t) => t.id === this.currentTab.id);
-  // }
 
   ngOnChanges() {
     if (this.dashboard && this.currentTab) {
@@ -34,7 +40,20 @@ export class Dashboard implements OnChanges {
     }
   }
 
+  showAddTabForm() {
+    this.isAddingTab.set(true);
+  }
+
   onTabChange(index: number): void {
     this.tabSelected.emit(this.dashboard.tabs[index].id);
+  }
+
+  cancelAddTab() {
+    this.isAddingTab.set(false);
+  }
+
+  addTab(title: string) {
+    if (!title || !title.trim()) return;
+    this.isAddingTab.set(false);
   }
 }
